87c7fc79d0de1a44fac3317c4922e1a5408c1bc2
lily <wl09720@ly.com>
2019-07-08 10:35:38 +0800

    公共方法

--- utils/index.js
+++ utils/index.js
@@ -155,6 +155,38 @@ const getDecimal = (value, number) => {
     return Number(value);
 }
 
+/**
+ * 列表页面防抖处理
+ * @param {*} url 
+ * @param {*} size 
+ */
+const throttle = (func, delay, mustRunDelay) => {
+
+    var startTime = null,
+        timer = null;
+    return (function() {
+        var context = this,
+            args = arguments,
+            currentTime;
+        clearTimeout(timer);
+        if (!startTime) {
+            startTime = Date.now();
+        }
+        currentTime = Date.now();
+        if (currentTime - startTime >= mustRunDelay) {
+            func.apply(context, args);
+            startTime = null;
+            clearTimeout(timer);
+        } else {
+            timer = setTimeout(function() {
+                func.apply(context, args);
+                startTime = null;
+                clearTimeout(timer);
+            }, delay);
+        }
+    })
+}
+
 
 module.exports = {
     extend,
@@ -167,4 +199,5 @@ module.exports = {
     isObject,
     isArray,
     getDecimal,
+    throttle,
 }

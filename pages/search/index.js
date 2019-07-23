5cd966b7a40460b46bb95759da310bf09a1bce91
lily <wl09720@ly.com>
2019-07-23 08:48:49 +0800

    update

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,4 +1,5 @@
-// pages/search/index.js
+import services from '../../services/index'
+
 Page({
 
     /**
@@ -8,10 +9,31 @@ Page({
         value: '',
     },
 
+    conditions(key, value) {
+        const query = new wx.BaaS.Query()
+        query.compare(key, '=', value)
+        return query
+    },
+
+    init() {
+        services.list({ table: 'books', limit: 6, query: this.conditions('isRecommend', true) }).then(res => {
+            const { meta, objects } = res
+            this.setData({ recommendList: objects })
+        })
+    },
+
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        this.init()
+    },
 
+    openDetail(e) {
+        const { id } = e.currentTarget.dataset
+
+        wx.navigateTo({
+            url: `/pages/detail/index?id=${id}`,
+        })
     },
 })

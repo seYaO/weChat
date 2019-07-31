c76331205cdbaabe91944b1f1a0bbfb9b3f4c8cd
lily <wl09720@ly.com>
2019-07-31 15:15:21 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,68 +1,29 @@
-// pages/index/index.js
+import services from '../../services/index'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
   },
 
-  init(){},
+  init(){
+        services.list({ table: 'store', limit: 50 }).then(res=>{
+            const {objects}=res
+            let arr = []
+            objects.map(item=>{
+                // {name:item.name,desc:item.desc}
+                arr.push(`${item.name} - ${item.desc}`)
+            })
+            console.log(JSON.stringify(arr))
+        })
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad (options) {
+      this.init()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
